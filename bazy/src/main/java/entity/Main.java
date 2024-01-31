fab5e3c57f0927f84ea77f78800e8f58f1a73063
jul.slov <143320479+slovik02@users.noreply.github.com>
2024-01-31 22:01:40 +0100

    Update Main.java

--- bazy/src/main/java/entity/Main.java
+++ bazy/src/main/java/entity/Main.java
@@ -4,27 +4,69 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Wybierz odpowiedni moduł: 1 - moduł lekarza, 2 - moduł technika");
-        int module = scan.nextInt();
-        System.out.println("Podaj swój nr ID:");
-        int ID = scan.nextInt();
-        System.out.println("Podaj hasło:");
-        String password = scan.nextLine();
-        if (module == 1){
+        Scanner scanner = new Scanner(System.in);
 
-        }
-        else if (module == 2){
+        System.out.print("Podaj nazwisko: ");
+        String surname = scanner.nextLine();
 
-        }
-        else {
-            System.out.println("Wybierz prawidłową opcję: 1 - moduł lekarza, 2 - moduł technika");
-            module = scan.nextInt();
-        }
+        System.out.print("Podaj hasło: ");
+        String password = scanner.nextLine();
 
+        String userType = authenticateUser(surname, password);
+
+        switch (userType) {
+            case "technik":
+                System.out.println("Zalogowano jako technik.");
+                // kod dla technika
+
+                break;
+            case "lekarz":
+                System.out.println("Zalogowano jako lekarz.");
+
+                // kod dla lekarza
+
+                break;
+            default:
+                System.out.println("Nieprawidłowe nazwisko lub hasło.");
+                break;
+        }
     }
 
+    private static String authenticateUser(String surname, String password) {
+        String url = "jdbc:mysql://localhost:3306/projekt";
+        String user = "root";
+        String dbPassword = "root";
 
+        try (Connection connection = DriverManager.getConnection(url, user, dbPassword)) {
+            // Sprawdź tabelę lekarzy
+            String queryDoctors = "SELECT 'lekarz' AS userType FROM doctors WHERE doctor_surname = ? AND doctor_password = ?";
+            try (PreparedStatement statement = connection.prepareStatement(queryDoctors)) {
+                statement.setString(1, surname);
+                statement.setString(2, password);
 
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    if (resultSet.next()) {
+                        return resultSet.getString("userType");
+                    }
+                }
+            }
 
+            // Sprawdź tabelę techników
+            String queryTechnicians = "SELECT 'technik' AS userType FROM technicians WHERE technician_surname = ? AND technician_password = ?";
+            try (PreparedStatement statement = connection.prepareStatement(queryTechnicians)) {
+                statement.setString(1, surname);
+                statement.setString(2, password);
+
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    if (resultSet.next()) {
+                        return resultSet.getString("userType");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return "nieprawidlowe";
+    }
 }
